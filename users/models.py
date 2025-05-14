6cb090b76e80cbe7838a659cd34cc034349877b0
rinshafebin <rinshafebinkk12@gmail.com>
2025-05-14 11:42:51 +0530

    first commit

---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=12)
+    address = models.TextField(blank=True,null=True)
+    profile_pictures = models.ImageField(upload_to='profile_pics/',blank=True,null=True)
+    
+    def __str__(self):
+        return self.    
